d4774c36da9e32b37c60f05b19f550a31c982fd2
Mahela Wickramasekara <mahela.amazonaws@gmail.com>
2021-05-14 14:16:06 +0530

    minor fixes

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-'use strict';
 
 console.log('Loading function');
 
-exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    const time_stamp = + new Date();
-    console.log('time stamp:',time_stamp);
-    //const { data } = await axios.get(`https://blockchain.info/blocks/${time_stamp}?format=json`);
-    //const dataSet = data?.blocks || [];
-    //const blocks = data && data.blocks ? data.blocks : [];
-    var AWS = require('aws-sdk');
-    AWS.config.update({region: 'us-east-1'});
+  const axios = require("axios");
+  const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
 
-    //for (let i = 0; i < blocks.length; i++) {
-        // publish to SNS topic
-        // Create publish parameters
-        var params = {
-          Message: 'Test Message from first lambda',
-          TopicArn: process.env.SNS_TOPIC_ARN
-          //TopicArn: 'arn:aws:sns:us-east-1:274047301157:sns-topic-lambda'
-        };
-        // Create promise and SNS service object
-        var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
-        // Handle promise's fulfilled/rejected states
-        publishTextPromise.then(
-          function(data) {
-            console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
-            console.log("MessageID is " + data.MessageId);
-          }).catch(
-            function(err) {
-            console.error(err, err.stack);
-          });
-        console.log('Sending Message');
-        //console.log(blocks[i]);
-    //}
+exports.handler = async (event) => {
+  const time_stamp = +new Date();
 
+  const sns = new SNSClient({ region: "us-east-1" });
+
+  const { data } = await axios.get(
+    `https://blockchain.info/blocks/${time_stamp}?format=json`
+  );
+  const dataSet = data && data.blocks ? data.blocks : [];
+
+  for (const d of dataSet) {
+    // publish to SNS topic
+    // Create publish parameters
+    var params = {
+      Message: JSON.stringify(d),
+      TopicArn: process.env.SNS_TOPIC_ARN,
+    };
+
+    try {
+      const data = await sns.send(new PublishCommand(params));
+      console.log("Message sent to the topic");
+      console.log("MessageID is " + data.MessageId);
+    } catch (err) {
+      console.error(err, err.stack);
+    }
+  }
+
+  return {
+      'statusCode': 200,
+      'headers': {'Content-Type': 'application/json'},
+      'body': "Execution finished successfully!"
+  }
 };
